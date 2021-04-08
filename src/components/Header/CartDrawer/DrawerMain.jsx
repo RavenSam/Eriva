242c86c09e498d5f584bd9d9cb14efb9822bfb0e
RavenSam <maksam7dev@gmail.com>
2021-04-08 13:28:16 +0100

    animation on umount of product from cart

--- src/components/Header/CartDrawer/DrawerMain.jsx
+++ src/components/Header/CartDrawer/DrawerMain.jsx
@@ -2,57 +2,60 @@ import { useState, useRef, Fragment } from "react";
 import { ShoppingCartOutline, XOutline } from "heroicons-react";
 import { useDispatch } from "react-redux";
 import { removeFromCart, adjustQty } from "src/redux/Shopping/shopping-actions";
-import { Divider, HStack,DrawerBody } from "@chakra-ui/react";
+import { motion, AnimatePresence } from "framer-motion";
+import { Divider, HStack, DrawerBody } from "@chakra-ui/react";
 
 export default function DrawerMain({ cart }) {
 	const dispatch = useDispatch();
 
 	return (
 		<>
-			<DrawerBody>
-				{cart.map((product) => (
-					<Fragment key={product.id}>
-						<div  className="drawerBody  w-full flex items-start justify-start">
-							<img src={product.image} alt="image" className="w-20" />
+			<DrawerBody overflowX="hidden">
+				<AnimatePresence>
+					{cart.map((product) => (
+						<motion.div  key={product.id} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0,x:"100%" }}>
+							<div className="drawerBody  w-full flex items-start justify-start py-2">
+								<img src={product.image} alt="image" className="w-20" />
 
-							<div className="Product px-2 pl-6 w-full">
-								<div className="flex items-center justify-between">
-									<h3 className="text-gray-700 text-sm font-heading font-semibold">{product.name}</h3>
+								<div className="Product px-2 pl-6 w-full">
+									<div className="flex items-start justify-between">
+										<h3 className="text-gray-700 text-sm font-heading font-semibold">{product.name}</h3>
 
-									<button
-										onClick={() => dispatch(removeFromCart(product.id))}
-										aria-label="Remove product"
-										type="button"
-										className="bg-white rounded-sm inline-flex items-center justify-center text-gray-400  focus:outline-none "
-									>
-										<XOutline size="1rem" />
-									</button>
-								</div>
-
-								<div className="flex items-center justify-between mt-4">
-									<HStack maxW="320px" border="1px solid #333">
 										<button
-											className="px-3 py-1 rounded-sm focus:outline-none"
-											onClick={() => product.qty && dispatch(adjustQty(product.id, product.qty - 1))}
+											onClick={() => dispatch(removeFromCart(product.id))}
+											aria-label="Remove product"
+											type="button"
+											className="bg-white rounded-sm inline-flex items-center justify-center text-gray-400  focus:outline-none "
 										>
-											-
+											<XOutline size="1rem" />
 										</button>
-										<div className="text-center">{product.qty}</div>
-										<button
-											className="px-3 py-1 rounded-sm focus:outline-none"
-											onClick={() => dispatch(adjustQty(product.id, product.qty + 1))}
-										>
-											+
-										</button>
-									</HStack>
+									</div>
 
-									<p className="text-base text-gray-600">${product.price}</p>
+									<div className="flex items-center justify-between mt-4">
+										<HStack maxW="320px">
+											<button
+												className="px-3 py-1 rounded-sm focus:outline-none"
+												onClick={() => product.qty && dispatch(adjustQty(product.id, product.qty - 1))}
+											>
+												-
+											</button>
+											<div className="text-center">{product.qty}</div>
+											<button
+												className="px-3 py-1 rounded-sm focus:outline-none"
+												onClick={() => dispatch(adjustQty(product.id, product.qty + 1))}
+											>
+												+
+											</button>
+										</HStack>
+
+										<p className="text-base text-gray-600">${product.price}</p>
+									</div>
 								</div>
 							</div>
-						</div>
-						<Divider mt=".5rem" mb="1rem" />
-					</Fragment>
-				))}
+							<Divider mt=".5rem" mb="1rem" />
+						</motion.div>
+					))}
+				</AnimatePresence>
 			</DrawerBody>
 		</>
 	);
