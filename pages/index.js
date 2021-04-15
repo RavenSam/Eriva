89561897f9be2c33159b82aa12bbb8378ff7877f
RavenSam <maksam7dev@gmail.com>
2021-04-15 16:47:02 +0100

    slider showcase & cleaning

--- pages/index.js
+++ pages/index.js
@@ -8,29 +8,21 @@ import ProductCard from "@components/Sections/ProductCard";
 import Card from "@components/shared/Card";
 
 // Dummy Data
-import { collections, slides } from "dummyData";
+import { collections, showcases } from "dummyData";
 
 export default function Home() {
   const { products } = useSelector((state) => state.shop);
 
   return (
     <>
-      <div className=" my-8 max-w-6xl mx-auto p-4">
+      <SliderImage items={showcases} />
 
+      <div className=" my-8 max-w-6xl mx-auto p-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 md:gap-4">
           {[1, 2, 3, 4, 5].map((item) => (
             <ProductCard key={item} />
           ))}
         </div>
-
-      </div>
-
-   
-
-      <SliderImage slides={slides} />
-
-      <div className="mt-4 mb-20">
-        <SlideCards slides={slides} />
       </div>
     </>
   );
--- src/components/Sections/SliderImage.jsx
+++ src/components/Sections/SliderImage.jsx
@@ -1,31 +1,51 @@
-import SwiperCore, { Navigation, Pagination, A11y, EffectFade, Autoplay } from "swiper"
-import { Swiper, SwiperSlide } from "swiper/react"
-import { isEmpty } from "lodash"
+import SwiperCore, { Navigation, Pagination, A11y, EffectFade, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { isEmpty } from "lodash";
 
-export default function SliderImage({ slides }) {
-   if (isEmpty(slides)) return null
+export default function SliderImage({ items }) {
+  if (isEmpty(items)) return null;
 
-   SwiperCore.use([Navigation, Pagination, A11y, EffectFade, Autoplay])
+  SwiperCore.use([Navigation, Pagination, A11y, EffectFade, Autoplay]);
 
-   return (
-      <>
-         <div className="sliderImage">
-            <Swiper spaceBetween={50} slidesPerView={1} navigation loop effect="fade" pagination={{ clickable: true }}>
-               {slides.map((slide) => (
-                  <SwiperSlide key={slide.label}>
-                     <div className="h-full bg-pink-400 flex flex-col items-center justify-center">
-                        <h2 className="text-center font-bold text-6xl">{slide.label}</h2>
-                     </div>
-                  </SwiperSlide>
-               ))}
-            </Swiper>
-         </div>
+  return (
+    <>
+      <div className="sliderImage">
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation
+          autoplay={{ delay: 5000 }}
+          loop
+          effect="fade"
+          pagination={{ clickable: true }}
+        >
+          {items.map((slide) => (
+            <SwiperSlide key={slide.id}>
+              <div
+                style={{ backgroundImage: `url(${slide.image})` }}
+                className="relative h-full bg-center flex flex-col items-center justify-center"
+              >
+                <div className="absolute top-0 left-0 w-full h-full bg-whiteAlpha-300" />
 
-         <style>{`
+                <div className="z-2 max-w-xl mx-auto">
+                  <h2
+                    style={{ textShadow: "2px 2px 2px rgba(206,89,55,0)" }}
+                    className="text-center font-black text-4xl uppercase text-primary-500"
+                  >
+                    {slide.text}
+                  </h2>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+
+      <style>{`
             .sliderImage .swiper-slide{
                height:450px;
             }
          `}</style>
-      </>
-   )
+    </>
+  );
 }
